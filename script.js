08e891edcbabf182bc173e5fb38294adb413f8a2
MupMcCool <160086048+MupMcCool@users.noreply.github.com>
2024-03-10 14:20:04 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let goodAve = 0;
 let evilAve = 0;
 
 // Fetch the power scores data
-fetch('path_to_powerScore.json')
+fetch(https://github.com/MupMcCool/Clocktower-Calculator/blob/bc32a880ad42803e00c51507d4250f6f61a6c40b/powerScore.json)
     .then(response => response.json())
     .then(data => localStorage.setItem('powerScores', JSON.stringify(data)));
 
